9f5c718879bcc6bfc09b348ff0ff578e371c008c
authorblues <authorblues@gmail.com>
2025-02-12 19:05:55 -0500

    set design analysis cleanup script

--- Other/SetDesignPoll-2024-Dec/00-cleanup.py
+++ Other/SetDesignPoll-2024-Dec/00-cleanup.py
@@ -40,9 +40,9 @@ with open('00-raw-deidentified.csv', 'r', newline='', encoding='utf8') as csvfil
 		for i in fav: favsets[i] = favsets.get(i, 0) + 1
 		output.append(
 			[
-				1 if "I am a player" in row[0] else 0,
-				1 if "I am a Developer" in row[0] else 0,
-				1 if "I volunteer" in row[0] else 0,
+				"I am a player" in row[0],
+				"I am a Developer" in row[0],
+				"I volunteer" in row[0],
 				masteryfocus.get(row[1], -1),
 				row[2],
 				', '.join(fav),
@@ -52,11 +52,11 @@ with open('00-raw-deidentified.csv', 'r', newline='', encoding='utf8') as csvfil
 			[
 				row[16],
 				milestonebreakdown.get(row[17], 0),
-				1 if "completion of all possible routes/endings, where applicable" in row[18] else 0,
-				1 if "100%, as defined by the game" in row[18] else 0,
-				1 if "further completion, as defined by the dev (collect all 1ups, collect all ammo packs, etc)" in row[18] else 0,
-				1 if "per-boss and/or per-stage challenges, as defined by the dev" in row[18] else 0,
-				1 if "full-game challenges, as defined by the dev" in row[18] else 0,
+				"completion of all possible routes/endings, where applicable" in row[18],
+				"100%, as defined by the game" in row[18],
+				"further completion, as defined by the dev (collect all 1ups, collect all ammo packs, etc)" in row[18],
+				"per-boss and/or per-stage challenges, as defined by the dev" in row[18],
+				"full-game challenges, as defined by the dev" in row[18],
 				int(row[19]) - 3,
 				agreement.get(row[20], 0),
 				agreement.get(row[21], 0),
@@ -69,53 +69,53 @@ with open('00-raw-deidentified.csv', 'r', newline='', encoding='utf8') as csvfil
 with open('01-processed.csv', 'w', newline='', encoding='utf8') as csvfile:
 	writer = csv.writer(csvfile)
 	writer.writerow([
-		'is-player', # 0 = no, 1 = yes
-		'is-dev', # 0 = no, 1 = yes
-		'is-volunteer', # 0 = no, 1 = yes
-		'how-focused-on-mastery', # 4 = high, 1 = low, 0 = not a player
-		'what-is-well-designed', # [open response]
-		'best-sets', # comma-separated list of RA set ids
-		'best-sets-why', # [open response]
+		'is_player', # 0 = no, 1 = yes
+		'is_dev', # 0 = no, 1 = yes
+		'is_volunteer', # 0 = no, 1 = yes
+		'how_focused_on_mastery', # 4 = high, 1 = low, 0 = not a player
+		'what_is_well_designed', # [open response]
+		'best_sets', # comma-separated list of RA set ids
+		'best_sets_why', # [open response]
 
 		# +2 love, +1 like, 0 neutral, -1 dislike, -2 hate/avoid
-		'achtype:progression', # Standard Progression & Milestones
-		'achtype:completion', # Completion (100%, collectables, all stages/missions)
-		'achtype:grinding', # Grinding / Hyper-Completion (reach level 99, collect every drop, reach 1000 kills with every weapon, etc)
-		'achtype:damageless-boss', # Damageless Boss Challenges
-		'achtype:deathless-boss', # Deathless Boss Challenges
-		'achtype:other-boss', # Other Boss Challenges (weapon restrictions, party restrictions, etc)
-		'achtype:level-challenges', # Level Challenges (no powerups, don't press left, no jumping, etc)
-		'achtype:easter-eggs', # Easter Eggs (highlighting one-off points of interest in a game, intended by the game developers)
-		'achtype:dev-challenges', # Developer-Created Challenges (highlighting one-off points of interest in a game, NOT intended by the game developers)
-		'achtype:score', # Score Challenges
-		'achtype:speedrun', # Speedrun Challenges
+		'achtype_progression', # Standard Progression & Milestones
+		'achtype_completion', # Completion (100%, collectables, all stages/missions)
+		'achtype_grinding', # Grinding / Hyper-Completion (reach level 99, collect every drop, reach 1000 kills with every weapon, etc)
+		'achtype_damageless_boss', # Damageless Boss Challenges
+		'achtype_deathless_boss', # Deathless Boss Challenges
+		'achtype_other_boss', # Other Boss Challenges (weapon restrictions, party restrictions, etc)
+		'achtype_level_challenges', # Level Challenges (no powerups, don't press left, no jumping, etc)
+		'achtype_easter_eggs', # Easter Eggs (highlighting one-off points of interest in a game, intended by the game developers)
+		'achtype_dev_challenges', # Developer-Created Challenges (highlighting one-off points of interest in a game, NOT intended by the game developers)
+		'achtype_score', # Score Challenges
+		'achtype_speedrun', # Speedrun Challenges
 
-		'other-enjoyed-achievements', # [open response]
-		'milestone-breakdown', # +1 individual achievements, 0 no opinion, -1 grouped achievements
+		'other_enjoyed_achievements', # [open response]
+		'milestone_breakdown', # +1 individual achievements, 0 no opinion, -1 grouped achievements
 
 		# 0 = no, 1 = yes
-		'set-expectation:all-endings', # completion of all possible routes/endings, where applicable
-		'set-expectation:completion', # 100%, as defined by the game
-		'set-expectation:full-collection', # further completion, as defined by the dev (collect all 1ups, collect all ammo packs, etc)
-		'set-expectation:boss-stage-challenges', # per-boss and/or per-stage challenges, as defined by the dev
-		'set-expectation:full-game-challenges', # full-game challenges, as defined by the dev
+		'set_expectation_all_endings', # completion of all possible routes/endings, where applicable
+		'set_expectation_completion', # 100%, as defined by the game
+		'set_expectation_full_collection', # further completion, as defined by the dev (collect all 1ups, collect all ammo packs, etc)
+		'set_expectation_boss_stage_challenges', # per-boss and/or per-stage challenges, as defined by the dev
+		'set_expectation_full_game_challenges', # full-game challenges, as defined by the dev
 
-		'willing-multiple-playthroughs' # -2 very unwilling, 0 neutral, +2 very willing
+		'willing_multiple_playthroughs', # -2 very unwilling, 0 neutral, +2 very willing
 
 		# +2 strongly agree, +1 agree, 0 neutral, -1 disagree, -2 strong disagree
-		'opinion:have-skipped-difficult', # I have skipped achievements for being too difficult
-		'opinion:have-skipped-by-type', # I have skipped achievements for being a type of achievement I don't like
-		'opinion:skipped-set', # I have not played *an entire set* because it contained a small number of achievements I didn't want to complete
-		'opinion:must-master', # I feel the need to master every set that I play, even if I don't care for some of the achievements
-		'opinion:single-genre-series', # I mostly play games of a specific series or genre
+		'opinion_have_skipped_difficult', # I have skipped achievements for being too difficult
+		'opinion_have_skipped_by_type', # I have skipped achievements for being a type of achievement I don't like
+		'opinion_skipped_set', # I have not played *an entire set* because it contained a small number of achievements I didn't want to complete
+		'opinion_must_master', # I feel the need to master every set that I play, even if I don't care for some of the achievements
+		'opinion_single_genre_series', # I mostly play games of a specific series or genre
 	])
 	for row in output:
 		writer.writerow(row)
 
+import requests
 with open('01-favorite-sets.txt', 'w') as f:
 	bycount = [(c, k) for k, c in favsets.items()]
 	for count, setid in reversed(sorted(bycount)):
-		import requests
 		import _credentials
 		import time
 
@@ -124,6 +124,6 @@ with open('01-favorite-sets.txt', 'w') as f:
 		if r.ok:
 			data = r.json()
 			name = f"{data['GameTitle']} ({data['ConsoleName']})"
-		time.sleep(0.5)
+		time.sleep(0.25)
 
 		f.write(f"{count} {setid} {name}\n")
