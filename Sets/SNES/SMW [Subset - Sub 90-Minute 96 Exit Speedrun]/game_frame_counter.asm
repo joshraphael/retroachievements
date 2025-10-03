be4f946b005a51f5c4ded4a5712b25b80905df37
authorblues <authorblues@gmail.com>
2025-10-02 20:38:49 -0400

    34233: 96 exit subset done, needs testing/badges

--- Sets/SNES/SMW [Subset - Sub 90-Minute 96 Exit Speedrun]/game_frame_counter.asm
+++ Sets/SNES/SMW [Subset - Sub 90-Minute 96 Exit Speedrun]/game_frame_counter.asm
@@ -1,6 +1,6 @@
 ; 32 bit frame counter that keeps incrementing as soon as the game starts.
 ; by Abdu
-; Credit: SMW Practice ROM
+; Credit: Fernap for helping me deal with nmi stuff.
 
 if read1($00FFD5) == $23
 	sa1rom
@@ -13,9 +13,7 @@ else
 endif
 
 ; 6 byte freeram, only 4 bytes are used. Cleared on reset and titlescreen load.
-!freeram = $0F3A|!addr
-!frame_counter = !freeram
-
+!frame_counter = $0F3A|!addr
 
 ; 4 byte freeram.
 ; Empty. Cleared on reset and titlescreen load. 
@@ -23,36 +21,80 @@ endif
 ; but this can be disabled with no known side effects by setting $00:9E48 to [80 01 EA].
 !backup_counter = $0DC3|!addr
 
-!counter_sixty_hz = $0F42
-!previous_sixty_hz = $0F43|!addr
-!real_frames = $0DDB|!addr
+; Postive = NMI Disabled, negative = NMI Enabled.
+!nmi_flag = $7F9C7B
 
-org $008176
-	jsr NMIStart_hijack
 
-; Overwrite some debug code for boss cutscene select.
-org $009510
-NMIStart_hijack:
-	inc !counter_sixty_hz
-	lda.w $4210
+org $008027
+	jsr init_nmi_flag
+
+org $00F9F5
+init_nmi_flag:
+	lda #$0080 : sta !nmi_flag ; high byte never being used so its fine.
+	lda #$F0A9
 rts
 
 
-org $008072
-	jsr game_loop_hijack
+; Repoint Native NMI vector.
+org $00FFEA
+	dw native_NMI_vector
+
+org $00FF93
+native_NMI_vector:
+	php
+	rep #$30
+	pha : phx : phy : phb
+	phk : plb
+	sep #$30
+
+	jsr handle_frame_counter
+	lda !nmi_flag : bpl .NMIDisabled
+	sei
+	; <Fernap>:
+	; expects a/x/y to be 8-bit...if that's not the case, 
+	; set it first, or jmp to $8174 instead
+	jmp $8176
+
+	.NMIDisabled:
+	rep #$30
+	plb : ply : plx : pla : plp
+rti
+
+
+org $00937D
+	jsr disable_nmi
+org $0096AE
+	jsr disable_nmi
+org $0096D5
+	jsr disable_nmi
+
+org $00BA4D
+disable_nmi:
+	lda #$01 : sta !nmi_flag
+	lda #$80 : sta $4200
+rts
+
+
+org $0082A1
+	jsr enable_nmi
+org $00838C
+	jsr enable_nmi
+org $0083C3
+	jsr enable_nmi
+org $0083F3
+	jsr enable_nmi
+org $0093F9
+	jsr enable_nmi
+
+org $00FA00
+enable_nmi:
+	sta.w $4200 : sta !nmi_flag
+rts
+
 
 ; Overwrite some unused code to make a running Yoshi at the left edge of the screen.
-; Previous hijack was at $009510, changed since there aren't enough bytes.
 org $00FC23
-game_loop_hijack:
-	; Run Gamemode code
-	jsr $9322
-	
-	; This part taken from SMW Practice ROM.
-	lda !counter_sixty_hz
-	sec : sbc !previous_sixty_hz : sta !real_frames
-	lda !counter_sixty_hz : sta !previous_sixty_hz
-
+handle_frame_counter:
 	; Make sure code only run in the correct gamemodes.
 	lda $0100|!addr : cmp #$0B : bcc .ret
 	jsr increment_frame_counter
@@ -62,7 +104,6 @@ game_loop_hijack:
 	lda !frame_counter : sta !backup_counter
 	lda !frame_counter+2 : sta !backup_counter+2
 	sep #$20
-
 	.ret:
 rts
 
@@ -72,7 +113,7 @@ org $00CC86
 increment_frame_counter:
 		; Now this is #peakcoding.
 		rep #$20
-		lda !frame_counter : clc : adc !real_frames
+		lda !frame_counter : inc
 	.check_low:
 		cmp #$FFFF : bcs .carry_low
 		sta !frame_counter
@@ -88,3 +129,4 @@ increment_frame_counter:
 	.done:
 		sep #$20
 rts
+
